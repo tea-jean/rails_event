cb3b94410e33351255948d574f74a2ca3e1430eb
tea-jean <tjholt@live.com.au>
2020-02-15 11:45:56 +1100

    added new views, defined controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,17 @@
 class EventsController < ApplicationController
-    before_action :find_event, only: (:show,:edit,:update,:destroy)
+    before_action :find_event, only: [:show, :edit, :update, :destroy]
+
     def index
         @events = Event.all
     end
+
     def show
     end
+
     def new
         @event = Event.new
     end
+
     def create
         @event = Event.new(event_params)
         if(@event.save)
@@ -16,14 +20,24 @@ class EventsController < ApplicationController
             render :new
         end
     end
+
     def edit
     end
+
     def update
+        if (@event.update)
+            redirect_to @event
+        else
+            render :edit
+        end
     end
-    def update
-    end
+
     def destroy
+        @event.delete
+
+        redirect_to event_path 
     end
+
 end
 
 private
@@ -36,4 +50,3 @@ def find_event
     @event = Event.find(params[:id])
 end
 
-end
